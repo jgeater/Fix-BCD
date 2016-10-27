5f7c3f0ba61fccd958fee07749137c905a7c246f
James Geater <jgeater@outlook.com>
2016-10-27 13:48:35 -0400

    initial commit
    clean the extra bootmanager entries diring imaging

---
+++ Fix-BCD.ps1
@@ -0,0 +1,48 @@
+﻿#This script will remove all but the current boot Firmware.
+#Mutiple boot firmware entries has been known to cause slow boots and failure to boot from a USB drive
+#This script requires admin rights.
+
+
+
+Function Get-BCDObject()
+{
+	$bcdEntries = bcdedit /enum firmware
+	$bcdObject = @()
+	
+	foreach ($bcdEntry in $bcdEntries)
+	{
+		
+		if ($bcdEntry -eq "Windows Boot Loader" -or $bcdEntry -eq "Windows Boot Manager" -or ($bcdEntry -eq "") -or $bcdEntry.Contains("-----"))
+		{
+			if ($bcdEntry.Contains("-----"))
+			{
+				$object1 = New-Object PSObject
+				$bcdObject += $object1
+                Write-host $object1
+			}
+		}
+		else
+		{
+			$bcdsplit = $bcdEntry.Split(" ")
+			$property = $bcdsplit[0]
+			$value = ($bcdEntry.Substring($bcdsplit[0].length)).trim()
+			
+			$object1 | Add-Member -MemberType NoteProperty -Name $($property) -Value $value
+		}
+	}
+	return $bcdObject
+}
+
+$bcdObjs = Get-BCDObject # Store bcdedit output in a Powershell object
+
+
+#Cleanup the extra entries and import the cleanup data
+foreach ($bcdObj in $bcdObjs)
+{
+	$BootIdentifier = $bcdObj.identifier
+	$BootDescription = $bcdObj.description
+	If ($BootDescription -eq "Windows Boot Loader" -or $BootDescription -eq "Windows Boot Manager" -and $BootIdentifier -ne "{bootmgr}")
+	{
+		bcdedit /delete $BootIdentifier /cleanup /f
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+When imaging a system (with win 8 or higher) several times you can get mutiple windows bootmanager entries in the windows boot manager.
+
+this script will clean the extra ones out leaving only one entry
+
+it is designed to be used during imaging of a system
